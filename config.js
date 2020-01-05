687d98f24dba3fe9b6e76c0692381e4f12b6ceda
hanull <dkfghg3@naver.com>
2020-01-05 16:44:36 +0900

    ddd 폴더 추가

--- config.js
+++ config.js
@@ -19,6 +19,10 @@ module.exports = {
       label: 'About me',
       path: '/pages/about'
     },
+    {
+      label: 'DomainDrivenDesign',
+      path: '/DDD/'
+    },
   ],
   author: {
     name: 'HST',
